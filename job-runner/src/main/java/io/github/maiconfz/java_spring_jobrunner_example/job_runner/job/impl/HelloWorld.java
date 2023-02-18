ae79f4f775819476849dcd225ee1f5d307b2a103
Maiconfz <maiconfz@gmail.com>
2023-02-18 10:42:52 +0000

    Change SimpleJob to just HelloWorld job

--- job-runner/src/main/java/io/github/maiconfz/java_spring_jobrunner_example/job_runner/job/impl/HelloWorld.java
+++ job-runner/src/main/java/io/github/maiconfz/java_spring_jobrunner_example/job_runner/job/impl/HelloWorld.java
@@ -4,19 +4,18 @@ import org.jobrunr.jobs.context.JobRunrDashboardLogger;
 import org.jobrunr.spring.annotations.Recurring;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.tinylog.Logger;
 
 import io.github.maiconfz.java_spring_jobrunner_example.job_runner.job.Job;
 
 @Component
-public class SimpleJob implements Job {
-    private static final org.slf4j.Logger JOBRUNNR_LOGGER = new JobRunrDashboardLogger(LoggerFactory.getLogger(SimpleJob.class));
+public class HelloWorld implements Job {
+    private static final org.slf4j.Logger JOBRUNNR_LOGGER = new JobRunrDashboardLogger(
+            LoggerFactory.getLogger(HelloWorld.class));
 
     @Override
-    @org.jobrunr.jobs.annotations.Job(name = "Simple Job")
-    @Recurring(id = "simple-job", cron = "*/1 * * * *")
+    @org.jobrunr.jobs.annotations.Job(name = "Hello World")
+    @Recurring(id = "hello-world-job", cron = "*/1 * * * *")
     public void doJob() {
-        Logger.info("SimpleJob...");
-        JOBRUNNR_LOGGER.info("SimpleJob");
+        JOBRUNNR_LOGGER.info("Hello World!");
     }
 }
