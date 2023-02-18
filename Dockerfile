a21e442d1efa0669f33fc1104d2fd6b0f2227bd6
Maiconfz <maiconfz@gmail.com>
2023-02-18 05:36:13 +0000

    Confire project to use postgres db, adding it to a container on docker compose; and configs inside de app

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ FROM eclipse-temurin:17-jre-alpine
 WORKDIR /opt/app
 COPY --from=builder /home/gradle/app/job-runner/build/libs/job-runner.jar /opt/app
 EXPOSE 9000
-CMD java -jar /opt/app/job-runner.jar --jobrunnerDbPassword=${POSTGRES_PASSWORD}
+CMD java -jar /opt/app/job-runner.jar
--- job-runner/src/main/java/io/github/maiconfz/java_spring_jobrunner_example/job_runner/config/JobRunrStorageConfiguration.java
+++ job-runner/src/main/java/io/github/maiconfz/java_spring_jobrunner_example/job_runner/config/JobRunrStorageConfiguration.java
@@ -1,21 +1,44 @@
 package io.github.maiconfz.java_spring_jobrunner_example.job_runner.config;
 
-import java.nio.file.Paths;
-
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.sqlite.SQLiteDataSource;
+import org.tinylog.Logger;
 
 @Configuration
 @ComponentScan(basePackageClasses = JobRunrStorageConfiguration.class)
 public class JobRunrStorageConfiguration {
 
-    @Bean
-    public SQLiteDataSource dataSource() {
-        final SQLiteDataSource dataSource = new SQLiteDataSource();
-        dataSource.setUrl("jdbc:sqlite:" + Paths.get(System.getProperty("java.io.tmpdir"), "jobrunr-example.db"));
-        return dataSource;
+    private final String serverName;
+    private final int port;
+    private final String dbName;
+    private final String user;
+    private final String password;
+
+    public JobRunrStorageConfiguration(@Value("${jobrunr.db.serverName}") String serverName,
+            @Value("${jobrunr.db.port}") int port, @Value("${jobrunr.db.dbName}") String dbName,
+            @Value("${jobrunr.db.user}") String user, @Value("${jobrunr.db.password}") String password) {
+        this.serverName = serverName;
+        this.port = port;
+        this.dbName = dbName;
+        this.user = user;
+        this.password = password;
+    }
+
+    @Bean(name = { "jobrunrDataSource" })
+    public PGSimpleDataSource dataSource() {
+        final PGSimpleDataSource ds = new PGSimpleDataSource();
+        ds.setServerNames(new String[] { this.serverName });
+        ds.setPortNumbers(new int[] { this.port });
+        ds.setDatabaseName(this.dbName);
+        ds.setUser(this.user);
+        ds.setPassword(this.password);
+
+        Logger.debug("jobrunrDataSource bean: {}", ReflectionToStringBuilder.toStringExclude(this, "password"));
+        return ds;
     }
 
 }
